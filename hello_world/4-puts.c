b101906437e680fdc5294758d0cf3764b8ac870a
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-13 04:28:41 -0700

    C program that prints a specific sentence

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
