2fecd166890516e09764dc22499446bbfec19b48
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-16 13:13:55 -0700

    Script that prints last digit of a random number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+/* your code goes there */
+	int y = n % 10;
+	if (y > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, y);
+	}
+	else if (y == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, y);
+	}
+	else if (y < 6 && y != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	}
+	return (0);
+}
