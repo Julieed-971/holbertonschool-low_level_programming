cd2918a8f2446c9ce3f28a5282383ad26977ae16
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-17 01:54:16 -0700

    Script that prints alphabet and skip specific characters

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
