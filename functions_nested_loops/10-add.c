a78e01e65b3301d8550b05a04bc0447351110370
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-19 06:59:54 -0700

    Function that prints two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - write the result of two integer addition
+ * @x: integer x
+ * @y: integer y
+ * Return: res
+ */
+
+int add(int x, int y)
+
+{
+	int res;
+
+	res = x + y;
+
+	return (res);
+
+}
