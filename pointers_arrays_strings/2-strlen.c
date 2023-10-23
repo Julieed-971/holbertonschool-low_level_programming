30407919d3a2a1658a54610060902f0f22a58fd5
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-23 08:11:33 -0700

    Return the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to char
+ * Return: count
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	return (count);
+}
