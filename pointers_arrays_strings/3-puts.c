ada1d90f3f198a0a172b58240f649591c4c47a99
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-23 08:41:14 -0700

    Write a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - write a string
+ * @str: pointer to a string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
