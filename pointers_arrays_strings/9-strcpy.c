c3405ccf3236eebea2339774e8404b70baa58f66
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-25 04:30:22 -0700

    Copy a string from a pointer to another

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy the string from a pointer to another
+ * * @src: pointer to a string
+ * @dest: pointer to a buffer$
+ * Return: *dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
