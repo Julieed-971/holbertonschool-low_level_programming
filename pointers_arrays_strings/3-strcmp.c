24269f0471d095411d2990778d7e41fb98eba9f0
Julieed-971 <julie.dedieu1@gmail.com>
2023-10-27 01:51:15 -0700

    Compare ASCII values of each character of two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,21 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
+	int i = 0;
 
-	while (s1[len1] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		len1++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	if (len1 < len2)
-		return (-15);
-	else if (len1 > len2)
-		return (15);
-	else
-		return (0);
+	return (0);
 }
