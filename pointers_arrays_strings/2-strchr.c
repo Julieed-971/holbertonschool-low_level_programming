6a9b22623cddaeecc32460e8eaaa421f92349a50
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-02 07:18:19 -0700

    Function that searches a string for any of a set of bytes

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,6 +11,14 @@
 
 char *_strchr(char *s, char c)
 {
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
 	{
 		if (c == '\0')
 		{
@@ -20,14 +28,6 @@ char *_strchr(char *s, char c)
 			}
 		}
 		return (s);
-	}
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
 	}
 	return (NULL);
 }
