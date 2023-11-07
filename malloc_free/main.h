55e323b966eee5b0850c1856c55d36099b9b5d71
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-07 00:36:58 -0800

    Updated header file

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strcat(char *dest, char *src)
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
