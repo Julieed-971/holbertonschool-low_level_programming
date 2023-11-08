338ffbd6b6afcae512f4779b18a12713935b2261
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-08 02:26:05 -0800

    Function that creates an array of chars

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i;
-
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	s = malloc(size * (sizeof(char)));
 	if (s == 0)
 	{
