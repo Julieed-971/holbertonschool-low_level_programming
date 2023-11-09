9cc287fb4fa5c42615bc4ac97584096ad411f11a
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-09 01:59:21 -0800

    Function that concatenate two strings with malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		len2++;
 	}
-	str = malloc(sizeof(char) * (len1 + len2) + sizeof(char));
+	str = malloc(sizeof(char) * (len1 + len2));
 	if (str == NULL)
 	{
 		return (NULL);
