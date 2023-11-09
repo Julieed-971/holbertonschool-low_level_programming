bcb4c613040ed76572509b62b2eb1ce01baf2159
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-09 01:45:22 -0800

    Header file

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <type.h>
+#include <stddef.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
