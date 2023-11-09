bfdead2d18b2c2c3daee04b8443fdbd174e8ca94
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-09 01:54:53 -0800

    Header file

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = ("");
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = ("");
 	}
 	while (s1[len1] != '\0')
 	{
