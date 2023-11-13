5463efc2ee0380753d5a90b405b899670f4e833d
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-13 01:59:12 -0800

    Structure for an ugly dog

---
+++ dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - ugly dog description
+ * @name: name of the ugly dog
+ * @age: age of the ugly dog
+ * @owner: name of the owner of the ugly dog
+ *
+ * Description: gives details about an ugly dog
+ */
+
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+
+}; 
+
+#endif
