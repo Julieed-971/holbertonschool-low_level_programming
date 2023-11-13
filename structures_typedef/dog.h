71fa128a428d43053c8cf5de91f1064da0ebfd53
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-13 02:19:09 -0800

    Structure for a dog description

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - dog description
@@ -14,4 +17,6 @@ struct dog
 		float age;
 		char *owner;
 }; 
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
