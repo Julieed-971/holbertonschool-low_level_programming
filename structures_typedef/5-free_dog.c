67e9acf6fc160d95b8484da5e69a168fb2ce4183
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-14 04:56:13 -0800

    Function that frees dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,19 +7,7 @@
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-	{
-		free(d);
-	}
-	if (d->name == NULL)
-	{
-		free(d->name);
-		free(d);
-	}
-	if (d->owner == NULL)
-	{
 		free(d->owner);
 		free(d->name);
 		free(d);
-	}
 }
