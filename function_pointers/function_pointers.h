8df1b9280f8596e33b75d0cee62a3ff2374f84f8
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-15 02:10:52 -0800

    Updated header file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_FOINTERS_H
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
