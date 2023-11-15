ead8b5328c6577fb42e16a360698f444adf51fc4
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-15 04:56:04 -0800

    Updated header file

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL)
+	if (array != NULL && *action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
