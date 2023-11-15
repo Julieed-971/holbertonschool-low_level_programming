c5efdca890ebe58bc9afd7be39085f071fdf2587
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-15 02:29:00 -0800

    New headerfile

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,4 @@
 #ifndef FUNCTION_POINTERS_H
-#define FUNCTION_FOINTERS_H
-#include <stdlib.h>
-#include <stddef.h>
-
-int _putchar(char c);
+#define FUNCTION_POINTERS_H
 void print_name(char *name, void (*f)(char *));
-
 #endif
