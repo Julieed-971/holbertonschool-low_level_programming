c193b953bb3041f10a08e92c220deaa10b6cb55a
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-15 04:48:20 -0800

    Function that iterate each elements of an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate an array
+ * @array: pointer to an array
+ * @size: size of the array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
