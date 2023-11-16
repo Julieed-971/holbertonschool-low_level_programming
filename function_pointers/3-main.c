5c377e152b0b613d455b2b4dc3c94dac36c150db
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-16 08:37:23 -0800

    Function that does not calculate what you think

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - calculate with two int input
+ * @argc: number of the argument
+ * @argv: array of arguments
+ *
+ * Return: a int of the result of the operation
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
+	char *op = argv[2];
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (op[0] != '+' && op[0] != '-' && op[0] != '*'
+			&& op[0] != '/' && op[0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((op[0] == '/' ||  op[0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
+
