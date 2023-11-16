89f79fccca840daa7cc9bdab3facd572dd286ddc
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-16 02:15:43 -0800

    Function that select the operation to perform

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * get_op_func - select the correct function to perform
+ * @s: pointer to char
+ *
+ * Return: pointer to function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
