4bb2beb67f8c1ff4dbb62385595101ca92f99ac7
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-16 00:55:45 -0800

    Functions that calculate

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - add two integers
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: int of the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two integers
+ * @a: first number to substract
+ * @b: second number to substract
+ *
+ * Return: int of the substraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two integers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: int of the multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divise two integers
+ * @a: first number to divise
+ * @b: second number to divise
+ *
+ * Return: int of the division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remain of the division of two integers
+ * @a: first number to modulo
+ * @b: second number to modulo
+ *
+ * Return: int of the modulo
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
