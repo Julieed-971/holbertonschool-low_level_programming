3b9a8afab5db740af3ef69b6e36273f641560d27
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-17 05:46:44 -0800

    Function that prints numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,11 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator == NULL)
-		{
-			printf("%s", "");
-		}
-		printf("%s", separator);
+		if (i < (n - 1) && separator != NULL)
+		printf("%s", separator);	
 	}
 	printf("\n");
 	va_end(args);
