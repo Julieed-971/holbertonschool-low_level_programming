10cbc3332589fc5f320d6607e35f9295ac55a6e0
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-18 11:47:13 -0800

    Added structure for the print_all function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,15 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-typedef struct format
+#include <stdarg.h>
+/**
+ * struct form - structure for format to print
+ *
+ * @c: first argument
+ * @format: function associated
+ */
+typedef struct form
 {
 	char *c;
 	void (*format)(va_list);
-};
+} format;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void form_char(va_list args);
+void form_int(va_list args);
+void form_float(va_list args);
+void form_str(va_list args);
 #endif
