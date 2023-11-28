9e43e0b6f276b1a07b344d6205676af74cb55626
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-27 23:32:53 -0800

    Function that adds a new node to the beginning of a list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
