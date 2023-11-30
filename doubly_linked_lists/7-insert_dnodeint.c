f6bcf918a212daa7cf955d41b4081cfbb0ebfed7
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-30 04:57:03 -0800

    Function that insert a node at a given index in a doubly linked list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,23 +12,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *tmp = *h, *ins_node;
 
-	if (idx == 0)
-	{
-		ins_node = malloc(sizeof(dlistint_t));
-		if (ins_node == NULL)
-		{
-			return (NULL);
-		}
-		ins_node->n = n;
-		ins_node->prev = NULL;
-		ins_node->next = *h;
-		if (*h != NULL)
-		{
-			(*h)->prev = ins_node;
-		}
-		*h = ins_node;
-		return (ins_node);
-	}
 	for (i = 0; i < idx - 1 && tmp != NULL; i++)
 		tmp = tmp->next;
 	if (tmp == NULL)
@@ -36,6 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	ins_node = malloc(sizeof(dlistint_t));
 	if (ins_node == NULL)
 	{
+		free(ins_node);
 		return (NULL);
 	}
 	ins_node->n = n;
