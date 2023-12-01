10e2b03c142bca76aadd88796216b45d1b76529a
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-01 01:48:52 -0800

    Function that deletes a node at a specific position

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,39 +12,34 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *del = *head;
-	dlistint_t *tmp = NULL;
+	dlistint_t *tmp;
 
-	if (del == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
-		del = del->next;
+		*head = del->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(del->prev);
-		del->prev = NULL;
 		return (1);
 	}
 	for (i = 0; del != NULL && i < index; i++)
 	{
 		del = del->next;
 	}
-	if (del->next == NULL)
-	{
-		while (del->next != NULL)
-		{
-			del = del->next;
-			tmp = del->prev;
-			tmp->next = NULL;
-			free(del);
-			return (1);
-		}
-	}
-	else
-	{
-		tmp = del->prev;
-		tmp->next = del->next;
+
+	if (del == NULL)
+		return (-1);
+
+	tmp = del->prev;
+	tmp->next = del->next;
+
+	if (del->next != NULL)
 		del->next->prev = tmp;
-		free(del);
-		del = NULL;
-	}
+
+	free(del);
+
 	return (1);
 }
