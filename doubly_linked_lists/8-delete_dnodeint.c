664dcf6eb2129625a1fbfb5dc89ca028f82e47d4
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-30 23:59:34 -0800

    Go get those last checks

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,19 +13,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *del = *head;
 
-	for (i = 0; del != NULL && i < index; i++)
-		del = del->next;
 	if (del == NULL)
 		return (-1);
-	if (index == 0)
+	for (i = 0; del != NULL && i < index; i++)
+		del = del->next;
 	{
-		*head = del->next;
+		if (index == 0)
+			*head = del->next;
+		if (del == *head)
+			*head = del->next;
+		if (del->next != NULL)
+			del->next->prev = del->prev;
+		if (del->prev != NULL)
+			del->prev->next = del->next;
 	}
-	if (del == *head)
-		*head = del->next;
-	if (del->next != NULL)
-		del->next->prev = del->prev;
-	if (del->prev != NULL)
-		del->prev->next = del->next;
 	return (1);
 }
