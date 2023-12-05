039bfb68c49f877a5399f1b270fb450cb3824a82
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-05 02:20:55 -0800

    Added library for file permissions

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
