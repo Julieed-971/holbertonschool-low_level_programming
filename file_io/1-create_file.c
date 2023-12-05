3bc16f1e43fffb438b51639a78cca7f4b56c76ee
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-05 03:34:06 -0800

    Function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: pointer to the file
+ * @text_content: pointer to the content of the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, num_write;
+	int len;
+
+	if (text_content != NULL)
+		len = strlen(text_content);
+	else
+		len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		num_write = write(fd, text_content, len);
+		if (num_write == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
