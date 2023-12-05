9ca17008e66962e70509d6f33428bc4213e0a210
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-05 05:44:34 -0800

    Function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,10 +21,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_EXCL | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content != NULL && text_content[0] != '\0')
 	{
 		num_write = write(fd, text_content, len);
 		if (num_write == -1)
