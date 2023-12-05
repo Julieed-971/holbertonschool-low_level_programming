189ed8b9c0011253fdc0d5051fe4ee03c6eab0fe
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-05 02:12:19 -0800

    Added string library

--- file_io/main.h
+++ file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
