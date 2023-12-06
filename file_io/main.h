a0e15710b2c5e6a182e8e6f50bc860108f018075
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-06 10:14:15 -0800

    Added stdio library and task 3 function prototypes

--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdarg.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
