ef4b1237bbeb18e078d2792a84bc9ac1e03814bf
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-06 10:12:36 -0800

    Added stdio library and task 3 function prototypes

--- file_io/main.h
+++ file_io/main.h
@@ -11,6 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(const char *file_from, const char *file_to);
-void manage_error(int exit_code, const char *error_message, const char *arg);
+void manage_error(int exit_code, const char *error_message, ...);
 int main(int ac, char **av);
 #endif
