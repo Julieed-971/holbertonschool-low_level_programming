31ea60c0b34755fab054f50309946a7572464819
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-06 09:46:00 -0800

    Added stdio library and task 3 function prototypes

--- file_io/main.h
+++ file_io/main.h
@@ -5,8 +5,12 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copy_file(const char *file_from, const char *file_to);
+void manage_error(int exit_code, const char *error_message, const char *arg);
+int main(int ac, char **av);
 #endif
