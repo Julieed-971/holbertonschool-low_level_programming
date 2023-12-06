71b0b927d6418815a01d71665648e568eeb48250
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-06 14:34:14 -0800

    Simplified code - removed variadic function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,89 +2,50 @@
 
 /**
  * copy_file - copy text from a file to another
- * @file_from: pointer to a file to copy from
- * @file_to: pointer to a file to copy to
- *
- * Return: 0 on success, 1, 2 or 3 on error
- */
-
-int copy_file(const char *file_from, const char *file_to)
-{
-	int fd1, fd2;
-	ssize_t num_read, num_write;
-	char buffer[1024];
-
-	if (file_from == NULL)
-		manage_error(98, "Error: Can't read from file %s\n", file_from);
-	fd1 = open(file_from, O_RDONLY);
-	if (fd1 == -1)
-	{
-		manage_error(98, "Error: Can't read from file %s\n", file_from);
-		close(fd1);
-	}
-	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd2 == -1)
-	{
-		manage_error(99, "Error: Can't write to %s\n", file_to);
-		close(fd2);
-	}
-	while ((num_read = read(fd1, buffer, sizeof(buffer))) > 0)
-	{
-		num_write = write(fd2, buffer, num_read);
-		if (num_read != num_write)
-		{
-			manage_error(99, "Error: Can't write to %s\n", file_to);
-			close(fd1);
-			close(fd2);
-		}
-	}
-	if (num_read == -1)
-	{
-		manage_error(98, "Error: Can't read from file %s\n", file_from);
-		close(fd1);
-		close(fd2);
-	}
-	if (close(fd1) == -1)
-		manage_error(100, "Error: Can't close fd %d\n", fd1);
-	if (close(fd2) == -1)
-		manage_error(100, "Error: Can't close fd %d\n", fd2);
-	return (0);
-}
-
-/**
- * manage_error - manages possible errors when copying a file to another
- * @exit_code: code number to âss depending on the error
- * @error_message: pointer to a error message text
- * @...: list of variable arguments
- */
-
-void manage_error(int exit_code, const char *error_message, ...)
-{
-	va_list args;
-
-	va_start(args, error_message);
-	dprintf(STDERR_FILENO, error_message, args);
-	va_end(args);
-
-	exit(exit_code);
-}
-
-/**
- * main - use the copy function and check for valid number of arguments
  * @ac: number of command line arguments
- * @av: pointer to the files
+ * @av: command line arguments
  *
  * Return: 0 on success
  */
 
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
+	int file_from, file_to;
+	ssize_t num_read, num_write;
+	char buffer[1024];
+
 	if (ac != 3)
-
-		manage_error(97, "Usage: cp file_from file_to\n", "");
-
-	copy_file(av[1], av[2]);
-
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(file_from), exit(99);
+	}
+	while ((num_read = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		num_write = write(file_to, buffer, num_read);
+		if (num_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(file_from);
+			close(file_to), exit(99);
+		}
+	}
+	if (num_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		close(file_from);
+		close(file_to), exit(98);
+	}
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
