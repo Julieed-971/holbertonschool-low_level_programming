4292ebca0f1b1746a0d3b8e694e4261f81a2e8f3
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-08 15:47:59 -0800

    Function that deletes a hast table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 
 	if (ht == NULL)
 		return;
-	for (i = 0; i < ht->size; i++)
+	for (; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
 		while (tmp != NULL)
@@ -24,5 +24,6 @@ void hash_table_delete(hash_table_t *ht)
 			tmp = next;
 		}
 	}
+	free(ht->array);
 	free(ht);
 }
