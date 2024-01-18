5dd75d5f4d029594ec5839a2d19af7804ded0709
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-18 10:28:19 +0100

    Function that searches for a value in an array

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+* linear_search - searches for a value in an array of int
+* @array: array to search for
+* @size: size of the array
+* @value: value to search
+*
+* Return:
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%ld] = [%d]", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
+		return (-1);
+	}
+}
